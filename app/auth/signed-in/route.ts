8111b88c607c2c3d5380a4fe01893a599ca071e4
Bartosz <janiukbartosz@gmail.com>
2025-07-07 19:43:54 +0200

    adjust route handler

--- app/auth/signed-in/route.ts
+++ app/auth/signed-in/route.ts
@@ -1,28 +1,26 @@
 import { prisma } from "@/lib/prisma/prisma";
 import { NextRequest, NextResponse } from "next/server";
 
-// In your API route (e.g., pages/api/webhooks/auth.js)
 export async function POST(req: NextRequest) {
 	const event = await req.json();
 
-	console.log("Evencik", event);
-	console.log("event.event", event.event);
-
-	// Check for email confirmation event
 	if (
 		event.type === "UPDATE" &&
 		event.table === "users" &&
 		event.record.email_confirmed_at
 	) {
 		try {
-			// Save user to your database
-			const user = await prisma.user.create({
-				data: {
+			await prisma.user.upsert({
+				where: { id: event.record.id },
+				update: {},
+				create: {
 					id: event.record.id,
 					email: event.record.email,
 				},
 			});
-			return NextResponse.json(user);
+			return NextResponse.json({
+				message: "Pomyślnie aktywowano konto uzytkownika",
+			});
 		} catch (error) {
 			console.error("Error saving user:", error);
 			return NextResponse.json({ error: "Failed to save user" });
